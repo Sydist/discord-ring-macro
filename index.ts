8346cd80f6c17465800b62523197aa344d3362ab
Sydist <sydistbot.personal@gmail.com>
2022-07-28 08:47:50 +0200

    Initial commit.

---
+++ index.ts
@@ -0,0 +1,36 @@
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+// ID of the channel to call in
+let dmID = "";
+
+// List of user IDs you want to ring
+let userIDs = [""];
+
+// Cooldown between ringing and canceling in milliseconds
+let cooldown = 50;
+
+export const setCooldown = (x: number) => cooldown = x;
+export const setUserIDs = (x: string[]) => userIDs = x;
+export const setDmID = (x: string) => dmID = x;
+
+
+const LINK = new URL(`https://discord.com/api/v9/channels/${dmID}/call/ring`);
+const CANCEL_LINK = new URL(`https://discord.com/api/v9/channels/${dmID}/call/stop-ringing`);
+
+const REQUEST: RequestInit = { 
+    method: "POST", 
+    headers: {
+        "authorization": Deno.env.get("TOKEN") as string,
+        "content-type": "application/json",
+    }, 
+    body: JSON.stringify({
+        "recipients": userIDs, 
+    }), 
+};
+
+while (true) {
+    await fetch(LINK, REQUEST);
+    await new Promise(resolve => setTimeout(resolve, cooldown));
+    await fetch(CANCEL_LINK, REQUEST);
+    await new Promise(resolve => setTimeout(resolve, cooldown));
+}
